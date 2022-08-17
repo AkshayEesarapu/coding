b9be6182b4a8c7aa97ebc3ba54699ca1bfa48caa
EESARAPU AKSHAY <69242056+AkshayEesarapu@users.noreply.github.com>
2022-08-17 19:49:36 +0530

    Update Sort of 0's ,1's and 2's.cpp

--- Arrays/Sort of 0's ,1's and 2's.cpp
+++ Arrays/Sort of 0's ,1's and 2's.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+         // code here 
+        
+        int low=0;
+        int high=a.size()-1;
+        int mid=0;
+        
+        while(mid<=high){
+            switch(a[mid]){
+                case 0:
+                    swap(a[mid++],a[low++]);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(a[high--],a[mid]);
+                    break;
+            }
+        }
+    }
+};
