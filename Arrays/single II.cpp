08aa19a2b7bf3d88f74217b3df416215977c2862
EESARAPU AKSHAY <69242056+AkshayEesarapu@users.noreply.github.com>
2022-08-20 11:14:32 +0530

    Create single II.cpp

---
+++ Arrays/single II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     int n=nums.size();
+        int p = 0;  
+           int q = 0;  
+           for(int i = 0; i<nums.size(); i++){  
+              p = q & (p ^ nums[i]);  
+              q = p | (q ^ nums[i]);  
+           }  
+           return q;  
+    }
+};
+   
