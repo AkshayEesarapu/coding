5cabc3c73979daf5d7ad10d2a09c370ceb443f59
EESARAPU AKSHAY <69242056+AkshayEesarapu@users.noreply.github.com>
2022-08-20 11:28:20 +0530

    Create employees with missing info.sql

---
+++ SQL_Problems/employees with missing info.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select employee_id 
+from Employees  where employee_id NOT IN (select employee_id from Salaries)
+UNION
+select employee_id
+from Salaries where employee_id NOT IN (select employee_id from Employees)
+order by employee_id
